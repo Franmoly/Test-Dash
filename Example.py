bd5d560f00f6b64f99ac6cb78881f1a3ff9ae4f5
Franmoly <146612288+Franmoly@users.noreply.github.com>
2024-07-24 15:52:56 -0500

    Add files via upload

---
+++ Example.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import plotly.express as px
+from dash import Dash, dcc, html
+from jupyter_dash import JupyterDash
+
+
+# Configurar la aplicación Dash
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+# Datos de ejemplo
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+# Crear la figura
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+# Definir el layout de la aplicación
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+    html.Div(children='''Dash: A web application framework for Python.'''),
+    dcc.Graph(id='example-graph', figure=fig)
+])
+
+
+# Ejecutar el servidor en modo "external"
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+
+
